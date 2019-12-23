1d666f908f18bf2791a99dc5e2b53df517bef2b6
https://github.com/Leotw <1123978215@qq.com>
2019-12-23 22:28:43 +0800

    update:添加环境变量

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,10 +4,17 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async server() {
-    const { ctx } = this;
+    const { ctx, app, config } = this;
+    const { url, client, server } = config.dsn;
+    const { key: clientKey, project: clientProject } = client;
+    const { key: serverKey, project: serverProject } = server;
     await ctx.render('app.js', {
       message: 'SSR Home',
-      url: ctx.url
+      url: ctx.url,
+      env: app.env,
+      version: config.version,
+      clientCfg: `http://${clientKey}@${url}/${clientProject}`,
+      serverCfg: `http://${serverKey}@${url}/${serverProject}`
     }, { markup: false });
   }
 
