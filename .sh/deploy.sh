bdf064bb39101e826ce5bdb50f1bbb080a42bafc
https://github.com/Leotw <1123978215@qq.com>
2019-12-23 22:28:13 +0800

    update:完善部署脚本

--- .sh/deploy.sh
+++ .sh/deploy.sh
@@ -3,9 +3,13 @@
 source ./.sh/prop.sh
 REMOTE_ADDRESS=${username}@${ip}
 
-# rsync -av --exclude=config/plugin.js ${localpath} ${REMOTE_ADDRESS}:${remotepath}/test
+# 登录远端
 ssh ${REMOTE_ADDRESS} rm -rf ${remotepath}/${appname}
+
+# 依据exclude.list配置筛选需要上传的文件
 rsync -av --exclude-from=.sh/exclude.list ${localpath} ${REMOTE_ADDRESS}:${remotepath}/
+
+# 压缩node_modules并上传
 tar -zcvf ./node_modules.tgz ./node_modules
 scp ./node_modules.tgz ${REMOTE_ADDRESS}:${remotepath}${appname}
 rm -rf ./node_modules.tgz
