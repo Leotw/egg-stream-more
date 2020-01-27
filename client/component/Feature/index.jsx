51dafa3bae2f2d6d3ef6b47e7b3d885175c0a440
https://github.com/Leotw <1123978215@qq.com>
2020-01-27 23:29:13 +0800

    update:划分组件

--- client/component/Feature/index.jsx
+++ client/component/Feature/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './style.less';
 
+/**
+ *
+ */
 class Feature extends React.Component {
   render() {
     const { name, desc, avatar } = this.props;
     return (
       <section className="Feature">
-        <div className="feature-item">
+        <div className="feature-item avatar-item">
           <img src={avatar} alt="avatar"/>
         </div>
         <div className="feature-item">
-          <div className="item-title small-text">{name}</div>
-          <div className="item-desc">{desc}</div>
+          <div className="item-title small-text normal-color">{name}</div>
+          <div className="item-desc mini-text gray-color">{desc}</div>
         </div>
       </section>
     )
--- client/container/Home/index.jsx
+++ client/container/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Link } from 'react-router-dom';
+import Feature from '../../component/Feature';
 import * as actions from './action';
 import './style.less';
 
@@ -12,19 +12,49 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ message: window.__INITIAL_STATE__.message })
+    console.log(this.props);
   }
 
   render() {
-    const { message } = this.state;
+    const { hospitalName, hospitalIcons, address, phone, serves } = this.props.store;
     return (
-      <div className="HomeContainer">
-        <article>{message}</article>
-        <div className="nav">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </div>
-      </div>
+      <section className="HomeContainer">
+        <section className="home-info-section normal-left-padding normal-right-padding">
+          <div className="hos-name medium-text normal-top-padding">{hospitalName}</div>
+          <div className="hos-property">
+            {
+              hospitalIcons && hospitalIcons.length && hospitalIcons.map((icon, id) => {
+                return (
+                  <span key={id} className="hos-icon mini-text">{icon}</span>
+                )
+              })
+            }
+          </div>
+          <div className="hos-address smaller-top-padding">{address}</div>
+          <div className="hos-link flex flex-wrap smaller-top-padding">
+            <div className="hos-phone-link link-btn mini-text" data-tel={phone}>{'电话'}</div>
+            <div className="hos-detail-link link-btn mini-text">{'详情'}</div>
+          </div>
+        </section>
+        <section className="serve-section">
+          <div className="serve-name small-text normal-left-padding normal-top-padding normal-bottom-padding">医疗服务</div>
+          <div className="serve-container">
+            {
+              serves && serves.length && serves.map((serve, id) => {
+                const { serveName, serveDesc, img } = serve;
+                return (
+                  <Feature
+                    key={id}
+                    name={serveName}
+                    desc={serveDesc}
+                    avatar={img}
+                  />
+                )
+              })
+            }
+          </div>
+        </section>
+      </section>
     );
   }
 }
@@ -37,6 +67,6 @@ const mapActionToProps = (dispatch) => {
   return {
     actions: bindActionCreators(actions, dispatch),
   };
-}
+};
 
 export default connect(mapStateToProps, mapActionToProps)(Home);
