a2d9745742f1b182aae8c3f2dbee813e0f0f80ac
https://github.com/Leotw <1123978215@qq.com>
2020-01-27 23:37:11 +0800

    update:首页服务端渲染

--- app/view/0.js
+++ app/view/0.js
@@ -1,2 +1,268 @@
-exports.ids=[0],exports.modules={14:function(e,n,t){var a=t(15),r=t(5),s="string"==typeof a?[[e.i,a,""]]:a;(n=e.exports=a.locals||{})._getContent=function(){return s},n._getCss=function(){return""+a},n._insertCss=function(e){return r(s,e)}},15:function(e,n,t){(n=t(4)(!1)).push([e.i,".Feature {\n  display: -webkit-box;\n  display: -webkit-flex;\n  display: flex;\n}\n.Feature .feature-item img {\n  width: 80%;\n}\n.Feature .avatar-item {\n  text-align: center;\n}\n",""]),e.exports=n},16:function(e,n,t){var a=t(17),r=t(5),s="string"==typeof a?[[e.i,a,""]]:a;(n=e.exports=a.locals||{})._getContent=function(){return s},n._getCss=function(){return""+a},n._insertCss=function(e){return r(s,e)}},17:function(e,n,t){(n=t(4)(!1)).push([e.i,".HomeContainer .home-info-section {\n  background: url('http://39.100.135.194/static/cms/img/bgHead.jpeg') center no-repeat;\n  background-size: cover;\n  padding-bottom: 5%;\n  color: #fff;\n}\n.HomeContainer .hos-icon {\n  display: inline-block;\n  background-color: #5292fb;\n  color: #fff;\n  margin-right: 1%;\n  padding: 0 0.5%;\n  border-radius: 2px;\n}\n.HomeContainer .serve-container {\n  display: -webkit-box;\n  display: -webkit-flex;\n  display: flex;\n  -webkit-flex-wrap: wrap;\n          flex-wrap: wrap;\n  padding: 0 2%;\n}\n.HomeContainer .serve-container .Feature {\n  width: 50%;\n  padding: 2% 0;\n}\n.HomeContainer .link-btn {\n  border: 1px solid #ffffff;\n  border-radius: 50px;\n  padding: 0 2%;\n  margin-right: 2%;\n}\n",""]),e.exports=n},19:function(e,n,t){"use strict";t.r(n);var a={};t.r(a),t.d(a,"fetch",(function(){return m})),t.d(a,"pull",(function(){return d}));var r=t(0),s=t.n(r),i=t(2),o=t(6);t(14);class l extends s.a.Component{render(){const{name:e,desc:n,avatar:t}=this.props;return s.a.createElement("section",{className:"Feature"},s.a.createElement("div",{className:"feature-item avatar-item"},s.a.createElement("img",{src:t,alt:"avatar"})),s.a.createElement("div",{className:"feature-item"},s.a.createElement("div",{className:"item-title small-text normal-color"},e),s.a.createElement("div",{className:"item-desc mini-text gray-color"},n)))}}var c=l;function m(){return{type:"FETCH"}}function d(){return{type:"PULL"}}t(16);class p extends s.a.Component{constructor(e){super(),this.state={}}componentDidMount(){console.log(this.props)}render(){const{hospitalName:e,hospitalIcons:n,address:t,phone:a,serves:r}=this.props.store;return s.a.createElement("section",{className:"HomeContainer"},s.a.createElement("section",{className:"home-info-section normal-left-padding normal-right-padding"},s.a.createElement("div",{className:"hos-name medium-text normal-top-padding"},e),s.a.createElement("div",{className:"hos-property"},n&&n.length&&n.map((e,n)=>s.a.createElement("span",{key:n,className:"hos-icon mini-text"},e))),s.a.createElement("div",{className:"hos-address smaller-top-padding"},t),s.a.createElement("div",{className:"hos-link flex flex-wrap smaller-top-padding"},s.a.createElement("div",{className:"hos-phone-link link-btn mini-text","data-tel":a},"电话"),s.a.createElement("div",{className:"hos-detail-link link-btn mini-text"},"详情"))),s.a.createElement("section",{className:"serve-section"},s.a.createElement("div",{className:"serve-name small-text normal-left-padding normal-top-padding normal-bottom-padding"},"医疗服务"),s.a.createElement("div",{className:"serve-container"},r&&r.length&&r.map((e,n)=>{const{serveName:t,serveDesc:a,img:r}=e;return s.a.createElement(c,{key:n,name:t,desc:a,avatar:r})}))))}}n.default=Object(i.connect)(e=>e,e=>({actions:Object(o.bindActionCreators)(a,e)}))(p)}};
+exports.ids = [0];
+exports.modules = {
+
+/***/ "./client/component/Feature/index.jsx":
+/*!********************************************!*\
+  !*** ./client/component/Feature/index.jsx ***!
+  \********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _style_less__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style.less */ "./client/component/Feature/style.less");
+/* harmony import */ var _style_less__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_style_less__WEBPACK_IMPORTED_MODULE_1__);
+
+
+/**
+ *
+ */
+
+class Feature extends react__WEBPACK_IMPORTED_MODULE_0___default.a.Component {
+  render() {
+    const {
+      name,
+      desc,
+      avatar
+    } = this.props;
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+      className: "Feature"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "feature-item avatar-item"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      src: avatar,
+      alt: "avatar"
+    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "feature-item"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "item-title small-text normal-color"
+    }, name), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "item-desc mini-text gray-color"
+    }, desc)));
+  }
+
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Feature);
+
+/***/ }),
+
+/***/ "./client/component/Feature/style.less":
+/*!*********************************************!*\
+  !*** ./client/component/Feature/style.less ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    var refs = 0;
+    var css = __webpack_require__(/*! !../../../node_modules/css-loader/dist/cjs.js??ref--6-1!../../../node_modules/postcss-loader/src??postcss!../../../node_modules/less-loader/dist/cjs.js??ref--6-3!./style.less */ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/less-loader/dist/cjs.js?!./client/component/Feature/style.less");
+    var insertCss = __webpack_require__(/*! ../../../node_modules/isomorphic-style-loader/insertCss.js */ "./node_modules/isomorphic-style-loader/insertCss.js");
+    var content = typeof css === 'string' ? [[module.i, css, '']] : css;
+
+    exports = module.exports = css.locals || {};
+    exports._getContent = function() { return content; };
+    exports._getCss = function() { return '' + css; };
+    exports._insertCss = function(options) { return insertCss(content, options) };
+
+    // Hot Module Replacement
+    // https://webpack.github.io/docs/hot-module-replacement
+    // Only activated in browser context
+    if (false) { var removeCss; }
+  
+
+/***/ }),
+
+/***/ "./client/container/Home/action.js":
+/*!*****************************************!*\
+  !*** ./client/container/Home/action.js ***!
+  \*****************************************/
+/*! exports provided: fetch, pull */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "fetch", function() { return fetch; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "pull", function() { return pull; });
+function fetch() {
+  return {
+    type: 'FETCH'
+  };
+}
+function pull() {
+  return {
+    type: 'PULL'
+  };
+}
+
+/***/ }),
+
+/***/ "./client/container/Home/index.jsx":
+/*!*****************************************!*\
+  !*** ./client/container/Home/index.jsx ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "react-redux");
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_redux__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! redux */ "redux");
+/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(redux__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _component_Feature__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../component/Feature */ "./client/component/Feature/index.jsx");
+/* harmony import */ var _action__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./action */ "./client/container/Home/action.js");
+/* harmony import */ var _style_less__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./style.less */ "./client/container/Home/style.less");
+/* harmony import */ var _style_less__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_style_less__WEBPACK_IMPORTED_MODULE_5__);
+
+
+
+
+
+
+
+class Home extends react__WEBPACK_IMPORTED_MODULE_0___default.a.Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    const {
+      hospitalName,
+      hospitalIcons,
+      address,
+      phone,
+      serves
+    } = this.props.store;
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+      className: "HomeContainer"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+      className: "home-info-section normal-left-padding normal-right-padding"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "hos-name medium-text normal-top-padding"
+    }, hospitalName), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "hos-property"
+    }, hospitalIcons && hospitalIcons.length && hospitalIcons.map((icon, id) => {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        key: id,
+        className: "hos-icon mini-text"
+      }, icon);
+    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "hos-address smaller-top-padding"
+    }, address), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "hos-link flex flex-wrap smaller-top-padding"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "hos-phone-link link-btn mini-text",
+      "data-tel": phone
+    }, '电话'), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "hos-detail-link link-btn mini-text"
+    }, '详情'))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+      className: "serve-section"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "serve-name small-text normal-left-padding normal-top-padding normal-bottom-padding"
+    }, "\u533B\u7597\u670D\u52A1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "serve-container"
+    }, serves && serves.length && serves.map((serve, id) => {
+      const {
+        serveName,
+        serveDesc,
+        img
+      } = serve;
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_component_Feature__WEBPACK_IMPORTED_MODULE_3__["default"], {
+        key: id,
+        name: serveName,
+        desc: serveDesc,
+        avatar: img
+      });
+    }))));
+  }
+
+}
+
+const mapStateToProps = state => {
+  return state;
+};
+
+const mapActionToProps = dispatch => {
+  return {
+    actions: Object(redux__WEBPACK_IMPORTED_MODULE_2__["bindActionCreators"])(_action__WEBPACK_IMPORTED_MODULE_4__, dispatch)
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_1__["connect"])(mapStateToProps, mapActionToProps)(Home));
+
+/***/ }),
+
+/***/ "./client/container/Home/style.less":
+/*!******************************************!*\
+  !*** ./client/container/Home/style.less ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    var refs = 0;
+    var css = __webpack_require__(/*! !../../../node_modules/css-loader/dist/cjs.js??ref--6-1!../../../node_modules/postcss-loader/src??postcss!../../../node_modules/less-loader/dist/cjs.js??ref--6-3!./style.less */ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/less-loader/dist/cjs.js?!./client/container/Home/style.less");
+    var insertCss = __webpack_require__(/*! ../../../node_modules/isomorphic-style-loader/insertCss.js */ "./node_modules/isomorphic-style-loader/insertCss.js");
+    var content = typeof css === 'string' ? [[module.i, css, '']] : css;
+
+    exports = module.exports = css.locals || {};
+    exports._getContent = function() { return content; };
+    exports._getCss = function() { return '' + css; };
+    exports._insertCss = function(options) { return insertCss(content, options) };
+
+    // Hot Module Replacement
+    // https://webpack.github.io/docs/hot-module-replacement
+    // Only activated in browser context
+    if (false) { var removeCss; }
+  
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/less-loader/dist/cjs.js?!./client/component/Feature/style.less":
+/*!*****************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--6-1!./node_modules/postcss-loader/src??postcss!./node_modules/less-loader/dist/cjs.js??ref--6-3!./client/component/Feature/style.less ***!
+  \*****************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+exports.push([module.i, ".Feature {\n  display: -webkit-box;\n  display: -webkit-flex;\n  display: flex;\n}\n.Feature .feature-item img {\n  width: 80%;\n}\n.Feature .avatar-item {\n  text-align: center;\n}\n", ""]);
+// Exports
+module.exports = exports;
+
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/less-loader/dist/cjs.js?!./client/container/Home/style.less":
+/*!**************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--6-1!./node_modules/postcss-loader/src??postcss!./node_modules/less-loader/dist/cjs.js??ref--6-3!./client/container/Home/style.less ***!
+  \**************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+exports.push([module.i, ".HomeContainer .home-info-section {\n  background: url('http://39.100.135.194/static/cms/img/bgHead.jpeg') center no-repeat;\n  background-size: cover;\n  padding-bottom: 5%;\n  color: #fff;\n}\n.HomeContainer .hos-icon {\n  display: inline-block;\n  background-color: #5292fb;\n  color: #fff;\n  margin-right: 1%;\n  padding: 0 0.5%;\n  border-radius: 2px;\n}\n.HomeContainer .serve-container {\n  display: -webkit-box;\n  display: -webkit-flex;\n  display: flex;\n  -webkit-flex-wrap: wrap;\n          flex-wrap: wrap;\n  padding: 0 2%;\n}\n.HomeContainer .serve-container .Feature {\n  width: 50%;\n  padding: 2% 0;\n}\n.HomeContainer .link-btn {\n  border: 1px solid #ffffff;\n  border-radius: 50px;\n  padding: 0 2%;\n  margin-right: 2%;\n}\n", ""]);
+// Exports
+module.exports = exports;
+
+
+/***/ })
+
+};;
 //# sourceMappingURL=0.js.map
--- client/container/Root.jsx
+++ client/container/Root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Async from '../component/Async';
+import Home from './Home';
 import '../asset/style/common.less';
 import '../asset/style/reset.less';
 
@@ -10,8 +11,8 @@ export default class Root extends React.Component {
       <div className="Container __root__">
         <Switch>
           <Route path="/about" component={Async(() => import('./About'))}/>
-          <Route path="/home" component={Async(() => import('./Home'))}/>
-          <Route path="/" component={Async(() => import('./Home'))}/>
+          <Route path="/home" component={Home}/>
+          <Route path="/" component={Home}/>
         </Switch>
       </div>
     )
